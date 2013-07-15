700917a5cc21f3797dc2ec3b10fd0221a1fd4d04
Joe Moon <joe@xoxomoon.com>
2013-07-15 14:08:17 -0700

    fix failing test

--- spec/classes/universal_access/enable_scrollwheel_zoom_spec.rb
+++ spec/classes/universal_access/enable_scrollwheel_zoom_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
-describe 'osx::universal_access::enable_scrollwheel_zoom' do
+describe 'osx::universal_access::scrollwheel_zoom' do
   let(:facts) { {:boxen_user => 'ilikebees'} }
+  let(:params) { {:enabled => true} }
 
   it do
     should contain_boxen__osx_defaults('Use mouse wheel (scroll gesture) to zoom').with({
       :key    => 'closeViewScrollWheelToggle',
       :domain => 'com.apple.universalaccess',
-      :value  => true,
+      :value  => params[:enabled],
       :user   => facts[:boxen_user]
     })
   end
