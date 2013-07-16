722ba70ef9113cde01376c2c45b13df4fac989c5
Joe Moon <joe@xoxomoon.com>
2013-07-16 01:02:22 -0700

    validate args

--- manifests/finder/icon_arrangement.pp
+++ manifests/finder/icon_arrangement.pp
@@ -1,4 +1,7 @@
-class osx::finder::icon_arrangement($style) {
+class osx::finder::icon_arrangement($style = 'grid') {
+
+  validate_re($style, '^(grid)$', "osx::disk_images::auto_mount([style] must be present or absent, is ${style}")
+
   exec { [
     "/usr/libexec/plistbuddy -c 'Set :DesktopViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
     "/usr/libexec/plistbuddy -c 'Set :FK_StandardViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
