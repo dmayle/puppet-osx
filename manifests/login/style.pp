d56a26bf05f398f7ff6494ca704a2bae98cc4224
Joe Moon <joe@xoxomoon.com>
2013-07-16 14:49:13 -0700

    default arg and arg validation

--- manifests/login/style.pp
+++ manifests/login/style.pp
@@ -1,8 +1,10 @@
-class osx::login::style($style) {
+class osx::login::style($style = 'name and password') {
 
-  case $style {
-    'name and password': { $style_code = 1 }
-    default:             { $style_code = $style }
+  validate_re($style, '^(name and password)$', "osx::login::style([ensure] must be one of the following: name and password, is ${style}")
+
+  $style_code = $style ? {
+    'name and password' => 1,
+    default => 1
   }
 
   boxen::osx_defaults { 'Set the default login screen style':
