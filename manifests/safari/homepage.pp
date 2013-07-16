8b1cc1c08e2b289155328b59c18e6c52f61e1ed8
Joe Moon <joe@xoxomoon.com>
2013-07-16 15:34:45 -0700

    default arg

--- manifests/safari/homepage.pp
+++ manifests/safari/homepage.pp
@@ -1,4 +1,5 @@
-class osx::safari::homepage($page) {
+class osx::safari::homepage($page = 'https://www.github.com') {
+
   boxen::osx_defaults { 'Set the Default Safari Homepage':
     user   => $::boxen_user,
     domain => 'com.apple.Safari',
