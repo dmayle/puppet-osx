ac59b670822cb744b77666c19509550ca3f98028
Joe Moon <joe@xoxomoon.com>
2013-07-15 18:06:28 -0700

    validate args and use defaulta arg in airport/join_mode_fallback

--- manifests/airport/join_mode_fallback.pp
+++ manifests/airport/join_mode_fallback.pp
@@ -2,6 +2,7 @@ class osx::airport::join_mode_fallback($mode) {
   include osx::airport
 
   # Modes: Prompt | JoinOpen | KeepLooking | DoNothing
+  validate_re($mode, '^(Prompt|JoinOpen|KeepLooking|DoNothing)$', "osx::airdrop::join_mode_fallback[mode] must be one of the following: Prompt, JoinOpen, KeepLooking, DoNothing; is ${mode}")
 
   exec { 'Set What to Do When None of the Preferred Networks Are Available':
     command => "${osx::airport::path_to_binary} prefs JoinModeFallback=${mode}",
