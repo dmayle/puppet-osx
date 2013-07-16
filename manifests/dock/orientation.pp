dab8286ecb030d1599e5437c01b4153c1c458729
Joe Moon <joe@xoxomoon.com>
2013-07-16 00:30:38 -0700

    refactor dock/orientation to use ensure, default arg, and validate arg

--- manifests/dock/orientation.pp
+++ manifests/dock/orientation.pp
@@ -1,5 +1,7 @@
-class osx::dock::orientation($orientation) {
+class osx::dock::orientation($orientation = 'bottom') {
   include osx::dock
+
+  validate_re($orientation, '^(top|right|bottom|left)$', "osx::dock::orientation([orientation] must be one of the following: top, right, bottom, left, is ${orientation}")
 
   boxen::osx_defaults { 'Set the Position of the Dock Relative to the Desktop':
     user   => $::boxen_user,
