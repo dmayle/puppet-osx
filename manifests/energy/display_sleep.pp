78349f0f4f64fe539e2542e3b9cd30f3ede8c5f2
Joe Moon <joe@xoxomoon.com>
2013-07-16 00:38:40 -0700

    validate args

--- manifests/energy/display_sleep.pp
+++ manifests/energy/display_sleep.pp
@@ -2,7 +2,7 @@ class osx::energy::display_sleep(
   $on_battery      = undef,
   $when_plugged_in = undef ) {
 
-  if $on_battery != undef {
+  if is_integer($on_battery) {
     exec { 'Set Time Until Display Sleeps When on Battery Power':
       command => "pmset -b displaysleep ${on_battery}",
       user    => root,
@@ -10,7 +10,7 @@ class osx::energy::display_sleep(
     }
   }
 
-  if $when_plugged_in != undef {
+  if is_integer($when_plugged_in) {
     exec { 'Set Time Until Display Sleeps When Plugged In':
       command => "pmset -c displaysleep ${when_plugged_in}",
       user    => root,
