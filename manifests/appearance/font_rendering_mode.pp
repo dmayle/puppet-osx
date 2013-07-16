e7ff8b43a407d4af2aaa59284ddd9a5b55e5b1a8
Joe Moon <joe@xoxomoon.com>
2013-07-15 22:35:24 -0700

    validate args

--- manifests/appearance/font_rendering_mode.pp
+++ manifests/appearance/font_rendering_mode.pp
@@ -3,11 +3,14 @@
 # Mode 2 enables subpixel font rendering on non-Apple LCDs
 #
 class osx::appearance::font_rendering_mode($mode) {
-  boxen::osx_defaults { 'Change the Rendering Mode Used to Display Text':
-    user   => $::boxen_user,
-    domain => 'NSGlobalDomain',
-    key    => 'AppleFontSmoothing',
-    type   => 'int',
-    value  => $mode,
+
+  if member([1, 2, 3], $mode) {
+    boxen::osx_defaults { 'Change the Rendering Mode Used to Display Text':
+      user   => $::boxen_user,
+      domain => 'NSGlobalDomain',
+      key    => 'AppleFontSmoothing',
+      type   => 'int',
+      value  => $mode,
+    }
   }
 }
